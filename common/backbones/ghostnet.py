8752ff0644894ee7650edabcbfa32d589a84a347
Xiaobin Zhang <david8862@gmail.com>
2021-07-02 16:00:25 +0800

    fix runtime error

--- common/backbones/ghostnet.py
+++ common/backbones/ghostnet.py
@@ -102,7 +102,7 @@ def SqueezeExcite(input_x, se_ratio=0.25, reduced_base_chs=None, divisor=4, name
 
     x = Conv2D(filters=reduce_chs, kernel_size=1, use_bias=True, name=name+'_conv_reduce')(x)
     x = ReLU(name=name+'_act')(x)
-    x = Conv2D(filters=input_x.shape[-1], kernel_size=1, use_bias=True, name=name+'_conv_expand')(x)
+    x = Conv2D(filters=int(input_x.shape[-1]), kernel_size=1, use_bias=True, name=name+'_conv_expand')(x)
 
     x = Activation(hard_sigmoid, name=name+'_hard_sigmoid')(x)
     x = Multiply()([input_x, x])
